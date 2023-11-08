c64a320803fa8a4e1575c1e331ac6444325f31d8
Pabl1k <poiumazaya@gmail.com>
2023-11-08 22:02:11 +0200

    Mobile view

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { DateRange, Filters } from "../Content/useContent.ts";
 import Input from "../Input/Input.tsx";
 import DateSelector from "../DateSelector/DateSelector.tsx";
 import Select from "../Select/Select.tsx";
+import menuIcon from "../../assets/icons/burgerMenu.svg";
+import closeIcon from "../../assets/icons/cross.svg";
 import "./Header.scss";
 
 interface Props {
@@ -28,29 +30,58 @@ const Header: FC<Props> = ({
   onSourceSelect,
   onClearFilters,
 }) => {
+  const [open, setOpen] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  const mobile = screenWidth <= 625;
+
+  const displayIcon = (
+    <button className="header__menu-container" onClick={() => setOpen(!open)}>
+      <img
+        src={open ? closeIcon : menuIcon}
+        className="header__menu-icon"
+        alt="menu icon"
+      />
+    </button>
+  );
+
+  const menu = (
+    <>
+      <button onClick={onClearFilters}>Clear all filters</button>
+      <Select
+        title="Source"
+        value={filters.source}
+        options={sources}
+        onSelect={onSourceSelect}
+      />
+      <Select
+        title="Category"
+        value={filters.category}
+        options={categories}
+        onSelect={onCategorySelect}
+      />
+      <DateSelector dateRange={filters.dateRange} onChange={onDateSearch} />
+      <Input
+        value={filters.keyword}
+        onChange={setKeywordFilter}
+        onSearch={onKeywordSearch}
+      />
+    </>
+  );
+
+  useEffect(() => {
+    const handleResize = () => setScreenWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="header">
       <div className="header__logo">NEWS LOGO</div>
       <div className="header__container">
-        <button onClick={onClearFilters}>Clear all filters</button>
-        <Select
-          title="Source"
-          value={filters.source}
-          options={sources}
-          onSelect={onSourceSelect}
-        />
-        <Select
-          title="Category"
-          value={filters.category}
-          options={categories}
-          onSelect={onCategorySelect}
-        />
-        <DateSelector dateRange={filters.dateRange} onChange={onDateSearch} />
-        <Input
-          value={filters.keyword}
-          onChange={setKeywordFilter}
-          onSearch={onKeywordSearch}
-        />
+        {mobile && displayIcon}
+        {mobile && open && menu}
+        {!mobile && menu}
       </div>
     </div>
   );
