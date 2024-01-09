73a09b15128c921343ab3e058569142d3fc1fc97
Pabl1k <poiumazaya@gmail.com>
2024-01-09 20:42:20 +0200

    card fix

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Result } from "../../api/types/model.ts";
+import { Result } from "../../api/types/response.ts";
 import "./Card.scss";
 
 interface Props {
@@ -7,41 +7,25 @@ interface Props {
 }
 
 const Card: FC<Props> = ({ data }) => {
-  const {
-    title,
-    description,
-    imageUrl,
-    sourceName,
-    sourceUrl,
-    publishedAt,
-    section,
-  } = data;
+  const { title, urlToImage, source, url, publishedAt } = data;
   const defaultImage =
-    sourceName === "The Guardian"
-      ? "https://birn.eu.com/wp-content/uploads/2018/11/guardian.png"
-      : "https://b1370787.smushcdn.com/1370787/wp-content/uploads/2020/11/SE-5000H-RWS-3.jpg?lossy=1&strip=1&webp=1";
+    "https://b1370787.smushcdn.com/1370787/wp-content/uploads/2020/11/SE-5000H-RWS-3.jpg?lossy=1&strip=1&webp=1";
 
-  const cardTitle = sourceUrl ? `Click to open in ${sourceName}` : undefined;
+  const cardTitle = url ? `Click to open in ${source.name}` : undefined;
 
   return (
     <div className="card" title={cardTitle}>
-      <a href={sourceUrl ?? ""} target="_blank">
+      <a href={url ?? ""} target="_blank">
         <img
           className="card__image"
-          src={imageUrl ?? defaultImage}
+          src={urlToImage ?? defaultImage}
           alt={title}
         />
         <div className="card__content">
           <span className="card__publication-day-or-section">
             Publication day: {publishedAt}
           </span>
-          {!!section && (
-            <span className="card__publication-day-or-section">
-              Category: {section}
-            </span>
-          )}
           <h2 className="card__title">{title}</h2>
-          <span className="card__description">{description}</span>
         </div>
       </a>
     </div>
