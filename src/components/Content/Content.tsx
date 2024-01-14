67058cd1187f884e9ccbcd9b86c653ffd7429378
Pabl1k <poiumazaya@gmail.com>
2024-01-14 14:47:22 +0200

    Skeleton fix

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,21 +1,22 @@
 import { FC } from "react";
 import Card from "../Card/Card.tsx";
 import Skeleton from "../Card/Skeleton.tsx";
-import { Result } from "../../api/types/model.ts";
 import "./Content.scss";
+import { Result } from "../../api/types/response.ts";
 
 interface Props {
-  data: Result[];
+  items: Result[];
   loading: boolean;
 }
 
-const Content: FC<Props> = ({ data, loading }) => {
+const Content: FC<Props> = ({ items, loading }) => {
   return (
     <div className="content">
-      {loading ? (
-        <Skeleton />
+      {loading && <Skeleton />}
+      {items.length ? (
+        items.map((item) => <Card key={item.url} data={item} />)
       ) : (
-        data.map((item) => <Card key={item.id} data={item} />)
+        <span>No news with the selected filters</span>
       )}
     </div>
   );
