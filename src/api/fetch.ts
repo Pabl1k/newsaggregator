e2d95db8c79c8f23faf1b74d2949f841aec856cf
Pabl1k <poiumazaya@gmail.com>
2024-01-23 19:16:32 +0200

    getData fix

--- src/api/fetch.ts
+++ src/api/fetch.ts
@@ -1,6 +1,6 @@
 import { Response, SourceResponse } from "./types/response.ts";
 import { pathCreator } from "./pathCreator.ts";
-import { AllParams, SourceParams, TopParams } from "./types/params.ts";
+import { Params, SourceParams } from "./types/params.ts";
 import { SearchInSource as Endpoint } from "./types/general.ts";
 
 type RequestEndpoint = "everything" | "top-headlines" | "top-headlines/sources";
@@ -16,10 +16,7 @@ const getEndpoint = (type: Endpoint): RequestEndpoint => {
 const key = import.meta.env.VITE_NEWS_API_KEY;
 const domain = "https://newsapi.org/v2";
 
-export const fetchData = async (
-  endpoint: Endpoint,
-  params?: AllParams | TopParams,
-) => {
+export const getData = async (endpoint: Endpoint, params: Params) => {
   const _endpoint = getEndpoint(endpoint);
   const _params = pathCreator(params) ?? "";
   const url = `${domain}/${_endpoint}${_params}&apiKey=${key}`;
@@ -27,12 +24,16 @@ export const fetchData = async (
   const response = await fetch(url);
   const data: Response = await response.json();
 
-  return data;
+  if (data.status === "error") {
+    alert(data.message);
+  }
+
+  return data.articles;
 };
 
-export const fetchSources = async (params?: SourceParams) => {
+export const getSources = async (params?: SourceParams) => {
   const _params = pathCreator(params) ?? "";
-  const url = `${domain}/sources?${_params}apiKey=${key}`;
+  const url = `${domain}/sources${_params}&apiKey=${key}`;
 
   const response = await fetch(url);
   const data: SourceResponse = await response.json();
